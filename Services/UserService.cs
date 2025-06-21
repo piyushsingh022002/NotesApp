eb69b100165da143b8f358a9c78f428a3f0aab22
Piyush Singh <b221037@iiit-bh.ac.in>
2025-06-22 00:40:27 +0530

    user-service-live-now-can-register-login

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,11 +1,31 @@
-﻿using System;
-namespace NotesApp.Services
-{
-	public class UserService
-	{
-		public UserService()
-		{
-		}
-	}
-}
+﻿using MongoDB.Driver;
+using NotesApp.Api.Models;
+using System.Threading.Tasks;
 
+namespace NotesApp.Api.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserService(MongoService mongoService)
+        {
+            _users = mongoService.GetCollection<User>("users");
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _users.Find(u => u.Email == email).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            await _users.InsertOneAsync(user);
+        }
+
+        public async Task<User?> GetByIdAsync(string id)
+        {
+            return await _users.Find(u => u.Id == id).FirstOrDefaultAsync();
+        }
+    }
+}
